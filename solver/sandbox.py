ed3e8e9833c0ee6222702a1511e4175f91b7d3df
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-05-21 14:12:00 +0200

    Tests des fonctions de contraintes

--- solver/sandbox.py
+++ solver/sandbox.py
@@ -1,51 +1,22 @@
 from z3 import Int, IntSort, Array, Solver, sat, ForAll, Implies, And, Exists, Lambda, If, RealSort, Const, Store, K
 import z3_datatypes as dt
-from constraints import *
-
-arr = Array('arr', IntSort(), IntSort())
-i = Const('i', dt.DateTimeInterval)
-j = Const('j', dt.DateTimeInterval)
-t = Array('m', dt.DateTimeInterval, dtypes.DateTimeInterval, dt.Slot)
-r = dt.Room.croom(-1, -1, False, False, False)
-i_0 = dt.DateTimeInterval.cdti(
-    dt.Datetime.cdt(20, 5, 2023, 15, 30),
-    dt.Datetime.cdt(20, 5, 2023, 16, 0)
-)
-i_1 = dt.DateTimeInterval.cdti(
-    dt.Datetime.cdt(100, 100, 100, 100, 100),
-    dt.Datetime.cdt(100, 100, 100, 100, 100)
-)
-sl = dt.Slot.cslot(
-    dt.Datetime.cdt(-1, -1, -1, -1, -1),
-    dt.Datetime.cdt(-1, -1, -1, -1, -1),
-    -1,
-    -1,
-    r
-)
-
-s = Solver()
-lm = Lambda([i, j],  If(
-    And(
-        ordered_datetimes(dt.start_time(i), dt.end_time(i)),
-        j != i
-    ),
-    dt.Slot.cslot(
-        dt.start_time(i),
-        dt.end_time(i),
-        0,
-        0,
-        r
-    ),
-    sl
-))
+import constraints
+import functions as funcs
 # print()
-s.add(t == lm)
+# s.add(t == lm)
+
+slots = [Const(f'sl{i}', dt.Slot) for i in range(4)]
+# positive_dt = [And(constraints.positive_datetime_values(dt.slot_start_time(sl)), constraints.positive_datetime_values(dt.slot_end_time(sl))) for sl in slots]
+formatted = [And(constraints.datetime_formatted(dt.slot_start_time(sl)), constraints.datetime_formatted(dt.slot_end_time(sl))) for sl in slots]
+ordered = [constraints.start_before_end(sl) for sl in slots]
+in_day_interval = [And(constraints.day_interval(sl, 8, 0, 18, 30)) for sl in slots]
+auto_exclusion = constraints.auto_exclusion(slots)
+s = Solver()
+
+s.add(*formatted, *ordered, *in_day_interval, *auto_exclusion)
 
 if s.check() == sat:
-    m = s.model()
-    # print(m)
-    print(f"{m.eval(m[t][i_0, i_1])}")
-    # for i in range(8):
-    #     print(f"m[{i}] = {m.eval(m[t][i])}")
+    # funcs.all_models(s, *ordered, *formatted, *in_day_interval, auto_exclusion)
+    print(s.model())
 else:
     print('Insatisfiable')
